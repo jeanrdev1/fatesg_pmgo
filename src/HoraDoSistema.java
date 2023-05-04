3bf8dd4fc991f17ac786d1fb1f91b157a3e9c728
Jean Ribeiro <develop.jeanribeiro@gmail.com>
2023-05-04 20:52:02 -0300

    primeiro commit

---
+++ src/HoraDoSistema.java
@@ -0,0 +1,20 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class HoraDoSistema {
+    public static void main(String[] args) {
+        LocalDateTime dateTime = LocalDateTime.now();
+
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        
+        System.out.println(dateFormat.format(dateTime));
+
+        if (dateTime.getHour() < 12) {
+            System.out.println("Bom Dia");
+        } else if (dateTime.getHour() < 18) {
+            System.out.println("Boa Tarde");
+        } else {
+            System.out.println("Boa Noite");
+        }
+    }
+}
